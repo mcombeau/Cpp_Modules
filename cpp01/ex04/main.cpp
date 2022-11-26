15bca738d9ab26302bd7b683b810f464e1ad0105
Mia <miacombeau@protonmail.com>
2022-11-26 16:30:59 +0100

    add copy/replace + testing

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,43 +3,94 @@
 #include <iostream>
 #include <fstream>
 
+void	printUsage(const char *name)
+{
+	std::cout << "Usage: " << name << " <filename> <string_to_replace>"
+		<< " <replacement_string>" << std::endl;
+	return ;
+}
+
+int	openSteams(std::ifstream *ifs, std::ofstream *ofs, std::string infile, std::string outfile)
+{
+	ifs->open(infile.c_str(), std::ifstream::in);
+	if (!ifs->good())
+	{
+		std::cerr << "Error: " << infile << ": "
+			<< strerror(errno) << std::endl;
+		return (-1);
+	}
+	ofs->open(outfile.c_str(), std::ofstream::out | std::ofstream::trunc);
+	if (!ofs->good())
+	{
+		ifs->close();
+		std::cerr << "Error: " << outfile << ": "
+			<< strerror(errno) << std::endl;
+		return (-1);
+	}
+	return (0);
+}
+
+void	replaceBuffer(std::string *buffer, std::string const target, std::string const subst)
+{
+	size_t	pos;
+
+	pos = 0;
+	if (target.empty())
+		return ;
+	while (pos != std::string::npos)
+	{
+		pos = buffer->find(target.c_str(), 0, target.length());
+		if (pos == std::string::npos)
+			break ;
+		buffer->erase(pos, target.length());
+		if (!subst.empty())
+			buffer->insert(pos, subst);
+	}
+}
+
+int	copyContents(std::ifstream *ifs, std::ofstream *ofs, std::string const target, std::string const subst)
+{
+	std::string	buffer;
+	
+	while (std::getline(*ifs, buffer))
+	{
+		if (ifs->fail())
+			return (-1);
+		replaceBuffer(&buffer, target, subst);
+		*ofs << buffer;
+		if (!ifs->eof())
+			*ofs << std::endl;
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
-	std::ifstream	ifs,
-					ofs;
+	std::ifstream	ifs;
+	std::ofstream	ofs;
 	std::string		infile,
 					outfile,
 					target,
-					replace;
+					subst;
 	if (ac != 4)
 	{
-		std::cout << "Usage: " << av[0] << " <filename> <string_to_replace>"
-			<< " <replacement_string>" << std::endl;
+		printUsage(av[0]);
 		return (1);
 	}
 	infile = av[1];
 	outfile = infile;
 	outfile.append(".replace");
 	target = av[2];
-	replace = av[3];
+	subst = av[3];
 
-	std::cout << "infile = " << infile << "\noutfile = " << outfile << std::endl;
-
-	ifs.open(infile.c_str(), std::ifstream::in);
-	if (!ifs.good())
+	if (openSteams(&ifs, &ofs, infile, outfile) == -1)
+		return (1);
+	if (copyContents(&ifs, &ofs, target, subst) == -1)
 	{
-		std::cerr << "Error: " << infile << ": "
-			<< strerror(errno) << std::endl;
+		ifs.close();
+		ofs.close();
 		return (1);
 	}
-	ofs.open(outfile.c_str(), std::ofstream::out | std::ofstream::trunc);
-	if (!ofs.good())
-	{
-		std::cerr << "Error: " << outfile << ": "
-			<< strerror(errno) << std::endl;
-		return (1);
-	}
-
 	ifs.close();
 	ofs.close();
 	return (0);
---
+++ cpp01/ex04/test.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+NAME="./loser_sed"
+TEST_DIR="./test"
+F_FORBIDDEN=$TEST_DIR"/forbidden"
+F_INEXISTANT="$TEST_DIR/does_not_exist"
+F_MAN_BASH="$TEST_DIR/man_bash"
+
+BOLD="\e[1m"
+CYAN="\e[33m"
+RESET="\e[0m"
+
+mkdir -p $TEST_DIR
+echo hello world > "$F_FORBIDDEN"
+chmod 000 $F_FORBIDDEN
+man bash | head -n 15 > $F_MAN_BASH
+
+
+echo -e "$BOLD----------- MAKE PROGRAM$RESET"
+echo
+make re
+echo
+
+echo -e "$BOLD----------- BEGIN TESTS$RESET"
+echo
+
+echo -e "$BOLD--- No args:$RESET"
+
+echo -e "$CYAN> $NAME$RESET"
+$NAME
+echo -e "$CYAN> $NAME $F_LOREM$RESET"
+$NAME $F_LOREM
+echo -e "$CYAN> $NAME $F_LOREM x$RESET"
+$NAME $F_LOREM x
+echo -e "$CYAN> $NAME $F_LOREM x y z$RESET"
+$NAME $F_LOREM x y z
+echo
+
+echo -e "$BOLD--- Non-existant file:$RESET"
+
+echo -e "$CYAN> $NAME $F_INEXISTANT x x$RESET"
+$NAME $F_INEXISTANT x x
+echo
+
+echo -e "$BOLD--- Forbidden file (chmod 000):$RESET"
+
+echo -e "$CYAN> $NAME $F_FORBIDDEN x x$RESET"
+$NAME $F_FORBIDDEN x x
+echo
+
+echo -e "$BOLD--- Replacements tests:$RESET"
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"\" \"\"$RESET"
+$NAME $F_MAN_BASH "" ""
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"\" \"x\"$RESET"
+$NAME $F_MAN_BASH "" "x"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"ash\" \"\"$RESET"
+$NAME $F_MAN_BASH "ash" ""
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \" \" \"x\"$RESET"
+$NAME $F_MAN_BASH " " "x"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \" \" \"[_]\"$RESET"
+$NAME $F_MAN_BASH " " "[_]"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"Bash is Copyright (C) 1989-2020 by the Free Software Foundation, Inc.\" \"ooo\"$RESET"
+$NAME $F_MAN_BASH "Bash is Copyright (C) 1989-2020 by the Free Software Foundation, Inc." "ooo"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"not_in_text_at_all\" \"ZZZ\"$RESET"
+$NAME $F_MAN_BASH "not_in_text_at_all" "ZZZ"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$CYAN> $NAME $F_MAN_BASH \"bash\" \"clash mash sash\"$RESET"
+$NAME $F_MAN_BASH "bash" "clash mash sash"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+rm -rf $TEST_DIR
