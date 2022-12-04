215fd52eff0c5cab9c790b1ab40097980e7d627e
Mia <miacombeau@protonmail.com>
2022-12-04 18:26:03 +0100

    rename for legibility

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 14:43:19 by mcombeau          #+#    #+#             */
-/*   Updated: 2022/12/04 18:22:16 by mcombeau         ###   ########.fr       */
+/*   Updated: 2022/12/04 18:25:35 by mcombeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@
 #include <string>
 
 int	main(void) {
-	std::string trapName0 = "SCVT-2000";
-	std::string trapName1 = "CT-42";
+	std::string scavTrapName = "SCVT-2000";
+	std::string clapTrapName = "CT-42";
 
-	ScavTrap	trap0(trapName0);
-	ClapTrap	trap1(trapName1);
+	ScavTrap	scavTrap(scavTrapName);
+	ClapTrap	clapTrap(clapTrapName);
 
-	trap1.attack(trapName0);
-	trap0.takeDamage(CLAPTRAP_DEFAULT_ATTACK_DAMAGE);
-	trap0.attack(trapName1);
-	trap1.takeDamage(SCAVTRAP_DEFAULT_ATTACK_DAMAGE);
-	trap1.beRepaired(5000);
-	trap0.guardGate();
+	clapTrap.attack(scavTrapName);
+	scavTrap.takeDamage(CLAPTRAP_DEFAULT_ATTACK_DAMAGE);
+	scavTrap.attack(clapTrapName);
+	clapTrap.takeDamage(SCAVTRAP_DEFAULT_ATTACK_DAMAGE);
+	clapTrap.beRepaired(5000);
+	scavTrap.guardGate();
 	for (int i = 0; i < 50; i++)
-		trap0.beRepaired(5000);
-	trap0.attack(trapName0);
-	trap0.guardGate();
-	trap0.takeDamage(5000);
+		scavTrap.beRepaired(5000);
+	scavTrap.attack(scavTrapName);
+	scavTrap.guardGate();
+	scavTrap.takeDamage(5000);
 	return (0);
 }
