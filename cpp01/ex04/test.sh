d5267a760e009089ebf1b36fbf6e368f709b233b
Mia Combeau <mcombeau@student.42.fr>
2022-12-12 16:06:09 +0100

    fix test

--- cpp01/ex04/test.sh
+++ cpp01/ex04/test.sh
@@ -7,7 +7,7 @@ F_INEXISTANT="$TEST_DIR/does_not_exist"
 F_MAN_BASH="$TEST_DIR/man_bash"
 
 BOLD="\e[1m"
-CYAN="\e[33m"
+YELLOW="\e[33m"
 RESET="\e[0m"
 
 mkdir -p $TEST_DIR
@@ -24,68 +24,70 @@ echo
 echo -e "$BOLD----------- BEGIN TESTS$RESET"
 echo
 
-echo -e "$BOLD--- No args:$RESET"
+echo -e "$BOLD--- Bad args:$RESET"
 
-echo -e "$CYAN> $NAME$RESET"
+echo -e "$YELLOW> $NAME$RESET"
 $NAME
-echo -e "$CYAN> $NAME $F_MAN_BASH$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH$RESET"
 $NAME $F_MAN_BASH
-echo -e "$CYAN> $NAME $F_MAN_BASH x$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH x$RESET"
 $NAME $F_MAN_BASH x
-echo -e "$CYAN> $NAME $F_MAN_BASH x y z$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH x y z$RESET"
 $NAME $F_MAN_BASH x y z
 echo
 
 echo -e "$BOLD--- Non-existant file:$RESET"
 
-echo -e "$CYAN> $NAME $F_INEXISTANT x x$RESET"
+echo -e "$YELLOW> $NAME $F_INEXISTANT x x$RESET"
 $NAME $F_INEXISTANT x x
 echo
 
 echo -e "$BOLD--- Forbidden file (chmod 000):$RESET"
 
-echo -e "$CYAN> $NAME $F_FORBIDDEN x x$RESET"
+echo -e "$YELLOW> $NAME $F_FORBIDDEN x x$RESET"
 $NAME $F_FORBIDDEN x x
 echo
 
-echo -e "$BOLD--- Replacements tests:$RESET"
+echo -e "$BOLD--- Replacements tests (Should NOT replace):$RESET"
 
-echo -e "$CYAN> $NAME $F_MAN_BASH \"\" \"\"$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"\" \"\"$RESET"
 $NAME $F_MAN_BASH "" ""
 diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
 echo
 
-echo -e "$CYAN> $NAME $F_MAN_BASH \"\" \"x\"$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"\" \"x\"$RESET"
 $NAME $F_MAN_BASH "" "x"
 diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
 echo
 
-echo -e "$CYAN> $NAME $F_MAN_BASH \"ash\" \"\"$RESET"
-$NAME $F_MAN_BASH "ash" ""
-diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
-echo
-
-echo -e "$CYAN> $NAME $F_MAN_BASH \" \" \"x\"$RESET"
-$NAME $F_MAN_BASH " " "x"
-diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
-echo
-
-echo -e "$CYAN> $NAME $F_MAN_BASH \" \" \"[_]\"$RESET"
-$NAME $F_MAN_BASH " " "[_]"
-diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
-echo
-
-echo -e "$CYAN> $NAME $F_MAN_BASH \"Bash is Copyright (C) 1989-2020 by the Free Software Foundation, Inc.\" \"ooo\"$RESET"
-$NAME $F_MAN_BASH "Bash is Copyright (C) 1989-2020 by the Free Software Foundation, Inc." "ooo"
-diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
-echo
-
-echo -e "$CYAN> $NAME $F_MAN_BASH \"not_in_text_at_all\" \"ZZZ\"$RESET"
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"not_in_text_at_all\" \"ZZZ\"$RESET"
 $NAME $F_MAN_BASH "not_in_text_at_all" "ZZZ"
 diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
 echo
 
-echo -e "$CYAN> $NAME $F_MAN_BASH \"bash\" \"clash mash sash\"$RESET"
+echo -e "$BOLD--- Replacements tests (SHOULD replace):$RESET"
+
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"ash\" \"\"$RESET"
+$NAME $F_MAN_BASH "ash" ""
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$YELLOW> $NAME $F_MAN_BASH \" \" \"x\"$RESET"
+$NAME $F_MAN_BASH " " "x"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$YELLOW> $NAME $F_MAN_BASH \" \" \"[_]\"$RESET"
+$NAME $F_MAN_BASH " " "[_]"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"by the Free Software Foundation, Inc.\" \"ooo\"$RESET"
+$NAME $F_MAN_BASH "by the Free Software Foundation, Inc." "ooo"
+diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
+echo
+
+echo -e "$YELLOW> $NAME $F_MAN_BASH \"bash\" \"clash mash sash\"$RESET"
 $NAME $F_MAN_BASH "bash" "clash mash sash"
 diff -s --color $F_MAN_BASH $F_MAN_BASH.replace
 echo
