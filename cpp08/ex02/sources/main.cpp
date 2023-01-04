786eb9b2f56b87ccd5b6cf4a0de1c93433b5029f
Mia <miacombeau@protonmail.com>
2023-01-04 15:05:56 +0100

    add and revise mutantStack tests

--- cpp08/ex02/sources/main.cpp
+++ cpp08/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mcombeau <mcombeau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 17:38:28 by mcombeau          #+#    #+#             */
-/*   Updated: 2023/01/03 19:19:42 by mcombeau         ###   ########.fr       */
+/*   Updated: 2023/01/04 15:04:57 by mcombeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,49 @@
 #include <list>
 #include <vector>
 
-#define RESET "\e[0m"
-#define BOLD "\e[1m"
-#define RED "\e[31m"
-#define GREEN "\e[32m"
-#define YELLOW "\e[33m"
-#define CYAN "\e[36m"
+#define RESET	"\e[0m"
+#define BOLD	"\e[1m"
+#define RED		"\e[31m"
+#define GREEN	"\e[32m"
+#define YELLOW	"\e[33m"
+#define BLUE	"\e[34m"
+#define PURPLE	"\e[35m"
+#define CYAN	"\e[36m"
 
 void	testSubject(void);
 void	testCharDequeMutantStack(void);
-void	testStringVectorMutantStack(void);
-void	testFloatListMutantStack(void);
+void	testIntListMutantStack(void);
+void	testStringListMutantStack(void);
+void	testFloatVectorMutantStack(void);
+
+template <typename T, typename C>
+void	printMutantStackSize(std::string name, MutantStack<T, C> mutantStack);
+template <typename T, typename C>
+void	printMutantStackTop(std::string name, MutantStack<T, C> mutantStack);
+template <typename T, typename C>
+void	printMutantStackWithIterator(std::string name, MutantStack<T, C> mutantStack);
+template <typename T, typename C>
+void	printMutantStackWithConstIterator(std::string name, MutantStack<T, C> const mutantStack);
+template <typename T, typename C>
+void	printMutantStackWithReverseIterator(std::string name, MutantStack<T, C> mutantStack);
+template <typename T, typename C>
+void	printMutantStackWithConstReverseIterator(std::string name, MutantStack<T, C> const mutantStack);
 
 int	main(void)
 {
 	::testSubject();
 	::testCharDequeMutantStack();
-	// ::testStringVectorMutantStack();
-	// ::testFloatListMutantStack();
+	::testFloatVectorMutantStack();
+	::testIntListMutantStack();
+	::testStringListMutantStack();
 	return 0;
 }
 
 void	testSubject(void)
 {
-	std::cout << std::endl << "---- SUBJECT TESTS" << std::endl;
+	std::cout << std::endl << "=========================================" << std::endl
+		<< "----          SUBJECT TESTS          ----" << std::endl
+		<< "=========================================" << std::endl;
 	{
 		std::cout << std::endl << "-- Mutant Stack:" << std::endl;
 		MutantStack<int> mstack;
@@ -92,16 +111,160 @@ void	testSubject(void)
 	std::cout << std::endl;
 }
 
-template <typename T>
-void	printMutantStackSize(std::string name, MutantStack<T> mutantStack)
+void	testCharDequeMutantStack(void)
+{
+	std::cout << std::endl << "=========================================" << std::endl
+		<< "----  MUTANT STACK CHAR DEQUE TESTS  ----" << std::endl
+		<<  "=========================================" << std::endl;
+	
+	MutantStack< char, std::deque<char> >	mutantStack;
+	std::cout << std::endl << GREEN "-- Created empty char mutant stack with deque container." RESET << std::endl;
+
+	::printMutantStackSize("MS", mutantStack);
+	for (int i = 0; i < 26; i++)
+		mutantStack.push('a' + i);
+	std::cout << std::endl << GREEN "-- Pushed alphabet to mutant stack with push()." RESET << std::endl;
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackWithIterator("MS", mutantStack);
+	::printMutantStackWithReverseIterator("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+
+	MutantStack< char, std::deque<char> > const	constMutantStack(mutantStack);
+	std::cout << std::endl << GREEN "-- Contructed const mutant stack copy." RESET << std::endl;
+	::printMutantStackSize("CMS", constMutantStack);
+	::printMutantStackWithConstIterator("CMS", constMutantStack);
+	::printMutantStackWithConstReverseIterator("CMS", constMutantStack);
+	::printMutantStackTop("CMS", constMutantStack);
+
+	std::cout << std::endl << GREEN "-- Emptying original mutant stack with pop()." RESET << std::endl;
+	for (int i = 0; i < 26; i++)
+		mutantStack.pop();
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+}
+
+void	testFloatVectorMutantStack(void)
+{
+	std::cout << std::endl << "=========================================" << std::endl
+		<< "---- MUTANT STACK FLOAT VECTOR TESTS ----" << std::endl
+		<<  "=========================================" << std::endl;
+	
+	MutantStack< float, std::vector<float> >	mutantStack;
+	std::cout << std::endl << GREEN "-- Created empty float mutant stack with vector container." RESET << std::endl;
+
+	::printMutantStackSize("MS", mutantStack);
+	for (int i = 1; i < 10; i++)
+		mutantStack.push(0.3f * i);
+	std::cout << std::endl << GREEN "-- Pushed floats to mutant stack with push()." RESET << std::endl;
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackWithIterator("MS", mutantStack);
+	::printMutantStackWithReverseIterator("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+
+	MutantStack< float, std::vector<float> > const	constMutantStack(mutantStack);
+	std::cout << std::endl << GREEN "-- Contructed const mutant stack copy." RESET << std::endl;
+	::printMutantStackSize("CMS", constMutantStack);
+	::printMutantStackWithConstIterator("CMS", constMutantStack);
+	::printMutantStackWithConstReverseIterator("CMS", constMutantStack);
+	::printMutantStackTop("CMS", constMutantStack);
+
+	std::cout << std::endl << GREEN "-- Erasing 6 elements from original mutant stack with pop()." RESET << std::endl;
+	for (size_t i = 0; i < 6; i++)
+		mutantStack.pop();
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+	::printMutantStackWithIterator("MS", mutantStack);
+
+	std::cout << std::endl << GREEN "-- Emptying all elements from original mutant stack with pop()." RESET << std::endl;
+	for (size_t i = 0; i < 3; i++)
+		mutantStack.pop();
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+}
+
+
+
+void	testIntListMutantStack(void)
+{
+	std::cout << std::endl << "=========================================" << std::endl
+		<< "----   MUTANT STACK INT LIST TESTS   ----" << std::endl
+		<< "=========================================" << std::endl;
+	
+	MutantStack< int, std::list<int> >	mutantStack;
+	std::cout << std::endl << GREEN "-- Created empty int mutant stack with list container." RESET << std::endl;
+
+	::printMutantStackSize("MS", mutantStack);
+	for (int i = 0; i < 10; i++)
+		mutantStack.push(i);
+	std::cout << std::endl << GREEN "-- Pushed numbers to mutant stack with push()." RESET << std::endl;
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackWithIterator("MS", mutantStack);
+	::printMutantStackWithReverseIterator("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+
+	MutantStack< int, std::list<int> > const	constMutantStack(mutantStack);
+	std::cout << std::endl << GREEN "-- Contructed const mutant stack copy." RESET << std::endl;
+	::printMutantStackSize("CMS", constMutantStack);
+	::printMutantStackWithConstIterator("CMS", constMutantStack);
+	::printMutantStackWithConstReverseIterator("CMS", constMutantStack);
+	::printMutantStackTop("CMS", constMutantStack);
+
+	std::cout << std::endl << GREEN "-- Emptying original mutant stack with pop()." RESET << std::endl;
+	for (size_t i = 0; i < 10; i++)
+		mutantStack.pop();
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+}
+
+void	testStringListMutantStack(void)
+{
+	std::cout << std::endl << "=========================================" << std::endl
+		<< "----  MUTANT STACK STRING LIST TESTS ----" << std::endl
+		<< "=========================================" << std::endl;
+	
+	MutantStack< std::string, std::list<std::string> >	mutantStack;
+	std::cout << std::endl << GREEN "-- Created empty string mutant stack with list container." RESET << std::endl;
+
+	::printMutantStackSize("MS", mutantStack);
+	mutantStack.push("Hello");
+	mutantStack.push("Coucou");
+	mutantStack.push("Hola");
+	mutantStack.push("Salutations");
+	mutantStack.push("Bonjour");
+	std::cout << std::endl << GREEN "-- Pushed strings to mutant stack with push()." RESET << std::endl;
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackWithIterator("MS", mutantStack);
+	::printMutantStackWithReverseIterator("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+
+	MutantStack< std::string, std::list<std::string> > const	constMutantStack(mutantStack);
+	std::cout << std::endl << GREEN "-- Contructed const mutant stack copy." RESET << std::endl;
+	::printMutantStackSize("CMS", constMutantStack);
+	::printMutantStackWithConstIterator("CMS", constMutantStack);
+	::printMutantStackWithConstReverseIterator("CMS", constMutantStack);
+	::printMutantStackTop("CMS", constMutantStack);
+
+	std::cout << std::endl << GREEN "-- Emptying original mutant stack with pop()." RESET << std::endl;
+	mutantStack.pop();
+	mutantStack.pop();
+	mutantStack.pop();
+	mutantStack.pop();
+	mutantStack.pop();
+	::printMutantStackSize("MS", mutantStack);
+	::printMutantStackTop("MS", mutantStack);
+}
+
+
+template <typename T, typename C>
+void	printMutantStackSize(std::string name, MutantStack<T, C> mutantStack)
 {
 	std::cout << name << " is empty: " << std::boolalpha << mutantStack.empty()
 		<< " (size = " << mutantStack.size() << ")." << std::endl;
 	return ;
 }
 
-template <typename T>
-void	printMutantStackTop(std::string name, MutantStack<T> mutantStack)
+template <typename T, typename C>
+void	printMutantStackTop(std::string name, MutantStack<T, C> mutantStack)
 {
 	std::cout << name << " top: ";
 	if (!mutantStack.empty())
@@ -112,110 +275,74 @@ void	printMutantStackTop(std::string name, MutantStack<T> mutantStack)
 	return ;
 }
 
-template <typename T>
-void	printMutantStackWithIterator(std::string name, MutantStack<T> mutantStack)
+template <typename T, typename C>
+void	printMutantStackWithIterator(std::string name, MutantStack<T, C> mutantStack)
 {
-	std::cout << "Iterator: " << name << " contents begin to end: ";
-	typename MutantStack<T>::iterator it = mutantStack.begin();
-	typename MutantStack<T>::iterator ite = mutantStack.end();
+	std::cout << CYAN "Iterator: " RESET << name << " contents begin to end: ";
+	typename MutantStack<T, C>::iterator it = mutantStack.begin();
+	typename MutantStack<T, C>::iterator ite = mutantStack.end();
 	for (; it != ite; ++it)
 		std::cout << "[" << *it << "] ";
 	std::cout << std::endl
-		<< "Iterator: " << name << " contents end to begin: ";
+		<< CYAN "Iterator: " RESET << name << " contents end to begin: ";
 	it = mutantStack.begin();
 	ite--;
-	for (; ite >= it; --ite)
+	it--;
+	for (; ite != it; --ite)
 		std::cout << "[" << *ite << "] ";
 	std::cout << std::endl;
 }
 
-template <typename T>
-void	printMutantStackWithConstIterator(std::string name, MutantStack<T> const mutantStack)
+template <typename T, typename C>
+void	printMutantStackWithConstIterator(std::string name, MutantStack<T, C> const mutantStack)
 {
-	std::cout << "Const iterator: " << name << " contents begin to end: ";
-	typename MutantStack<T>::const_iterator it = mutantStack.begin();
-	typename MutantStack<T>::const_iterator ite = mutantStack.end();
+	std::cout << YELLOW "Const iterator: " RESET << name << " contents begin to end: ";
+	typename MutantStack<T, C>::const_iterator it = mutantStack.begin();
+	typename MutantStack<T, C>::const_iterator ite = mutantStack.end();
 	for (; it != ite; ++it)
 		std::cout << "[" << *it << "] ";
 	std::cout << std::endl
-		<< "Const iterator: " << name << " contents end to begin: ";
+		<< YELLOW "Const iterator: " RESET << name << " contents end to begin: ";
 	it = mutantStack.begin();
 	ite--;
-	for (; ite >= it; --ite)
+	it--;
+	for (; ite != it; --ite)
 		std::cout << "[" << *ite << "] ";
 	std::cout << std::endl;
 }
 
-template <typename T>
-void	printMutantStackWithReverseIterator(std::string name, MutantStack<T> mutantStack)
+template <typename T, typename C>
+void	printMutantStackWithReverseIterator(std::string name, MutantStack<T, C> mutantStack)
 {
-	std::cout << "Reverse iterator: " << name << " contents rbegin to rend: ";
-	typename MutantStack<T>::reverse_iterator itr = mutantStack.rbegin();
-	typename MutantStack<T>::reverse_iterator itre = mutantStack.rend();
+	std::cout << BLUE "Reverse iterator: " RESET << name << " contents rbegin to rend: ";
+	typename MutantStack<T, C>::reverse_iterator itr = mutantStack.rbegin();
+	typename MutantStack<T, C>::reverse_iterator itre = mutantStack.rend();
 	for (; itr != itre; ++itr)
 		std::cout << "[" << *itr << "] ";
 	std::cout << std::endl
-		<< "Reverse iterator: " << name << " contents rend to rbegin: ";
+		<< BLUE "Reverse iterator: " RESET << name << " contents rend to rbegin: ";
 	itr = mutantStack.rbegin();
 	itre--;
-	for (; itre >= itr; --itre)
+	itr--;
+	for (; itre != itr; --itre)
 		std::cout << "[" << *itre << "] ";
 	std::cout << std::endl;
 }
 
-template <typename T>
-void	printMutantStackWithConstReverseIterator(std::string name, MutantStack<T> const mutantStack)
+template <typename T, typename C>
+void	printMutantStackWithConstReverseIterator(std::string name, MutantStack<T, C> const mutantStack)
 {
-	std::cout << "Const reverse iterator: " << name << " contents rbegin to rend: ";
-	typename MutantStack<T>::const_reverse_iterator itr = mutantStack.rbegin();
-	typename MutantStack<T>::const_reverse_iterator itre = mutantStack.rend();
+	std::cout << PURPLE "Const reverse iterator: " RESET << name << " contents rbegin to rend: ";
+	typename MutantStack<T, C>::const_reverse_iterator itr = mutantStack.rbegin();
+	typename MutantStack<T, C>::const_reverse_iterator itre = mutantStack.rend();
 	for (; itr != itre; ++itr)
 		std::cout << "[" << *itr << "] ";
 	std::cout << std::endl
-		<< "Const reverse iterator: " << name << " contents rend to rbegin: ";
+		<< PURPLE "Const reverse iterator: " RESET << name << " contents rend to rbegin: ";
 	itr = mutantStack.rbegin();
 	itre--;
-	for (; itre >= itr; --itre)
+	itr--;
+	for (; itre != itr; --itre)
 		std::cout << "[" << *itre << "] ";
 	std::cout << std::endl;
-}
-
-void	testCharDequeMutantStack(void)
-{
-	std::cout << std::endl << "---- MUTANT STACK CHAR DEQUE TESTS" << std::endl;
-	
-	MutantStack< char, std::deque<char> >	mutantStack;
-	std::cout << std::endl << "-- Created empty char mutant stack with deque container." << std::endl;
-
-	::printMutantStackSize("MS", mutantStack);
-	for (int i = 0; i < 26; i++)
-		mutantStack.push('a' + i);
-	std::cout << std::endl << "-- Pushed alphabet to mutant stack with push()." << std::endl;
-	::printMutantStackSize("MS", mutantStack);
-	::printMutantStackWithIterator("MS", mutantStack);
-	::printMutantStackWithReverseIterator("MS", mutantStack);
-	::printMutantStackTop("MS", mutantStack);
-
-	MutantStack< char, std::deque<char> > const	constMutantStack(mutantStack);
-	std::cout << std::endl << "-- Contructed const mutant stack copy." << std::endl;
-	::printMutantStackSize("CMS", constMutantStack);
-	::printMutantStackWithConstIterator("CMS", constMutantStack);
-	::printMutantStackWithConstReverseIterator("CMS", constMutantStack);
-	::printMutantStackTop("CMS", constMutantStack);
-
-	std::cout << std::endl << "-- Emptying original mutant stack with pop()." << std::endl;
-	for (int i = 0; i < 26; i++)
-		mutantStack.pop();
-	::printMutantStackSize("MS", mutantStack);
-	::printMutantStackTop("MS", mutantStack);
-}
-
-void	testStringVectorMutantStack(void)
-{
-
-}
-
-void	testFloatListMutantStack(void)
-{
-
 }
